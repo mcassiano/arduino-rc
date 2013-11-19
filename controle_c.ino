21241596001afdb9c3645db91b5f8ff2004490c1
Matheus Cassiano <matheus@mcassiano.com>
2013-11-19 14:52:28 -0200

    Added support to MPlayerX

--- controle_c.ino
+++ controle_c.ino
@@ -1,44 +1,49 @@
-  /*
+/*
    * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
-   * An IR detector/demodulator must be connected to the input RECV_PIN.
-   * Version 0.1 July, 2009
-   * Copyright 2009 Ken Shirriff
-   * http://arcfn.com
-   */
-  #include <IRremote.h>
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+#include <IRremote.h>
 
-   int RECV_PIN = 11;
+int RECV_PIN = 11;
 
-   IRrecv irrecv(RECV_PIN);
+IRrecv irrecv(RECV_PIN);
 
-   decode_results results;
+decode_results results;
 
-   void setup()
-   {
-    Serial.begin(9600);
-    irrecv.enableIRIn(); // Start the receiver
-  }
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
 
-  void loop() {
-    if (irrecv.decode(&results)) {
-
-      if (results.value == 16720605)
-        Serial.println("previous");
-
-      if (results.value == 16712445)
-        Serial.println("next");
-
-      if (results.value == 16761405)
-        Serial.println("play");
-
-      if (results.value == 16754775)
-        Serial.println("vup");  
-
-      if (results.value == 16769055)
-        Serial.println("vdown");
-                
-       delay(500);
+void loop() {
+  if (irrecv.decode(&results)) {
       
-        irrecv.resume(); // Receive the next value
-      }
-    }
+    if (results.value == 0xFF0008F7)
+      Serial.println("previous");
+
+    if (results.value == 0xFF0058A7)
+      Serial.println("next");
+
+    if (results.value == 0xFF009867)
+      Serial.println("play");
+
+    if (results.value == 0xFF00E817)
+      Serial.println("vup");  
+
+    if (results.value == 0xFF00D827) 
+      Serial.println("vdown");
+      
+     if (results.value == 0xFF0018E7)
+      Serial.println("fullscreen");
+      
+     if (results.value == 0xFF00D02F)
+      Serial.println("subtitle");
+
+    irrecv.resume(); // Receive the next value
+  }
+}
+
--- looper.py
+++ looper.py
@@ -1,7 +1,62 @@
 import serial
 import os
+import subprocess
 
-ser = serial.Serial('/dev/tty.usbserial-A501IFHH', 9600, timeout=1)
+ser = serial.Serial('/dev/tty.usbserial-A501IFHH', 9600)
+
+play_pause = '''
+property lastPaused : "%s"
+tell application "iTunes" to set itunesState to (player state as text)
+tell application "MPlayerX" to set mplayerxState to ((playstatus) as text)
+
+if itunesState is equal to "playing" then
+	tell application "iTunes" to playpause
+	set lastPaused to "iTunes"
+else if ((itunesState is equal to "paused") and (lastPaused is equal to "iTunes")) then
+	tell application "iTunes" to playpause
+else if mplayerxState is equal to "Playing" then
+	tell application "MPlayerX" to playpause
+	set lastPaused to "MPlayerX"
+else if ((mplayerxState is equal to "Paused") and (lastPaused is equal to "MPlayerX")) then
+	tell application "MPlayerX" to playpause
+end if
+'''
+
+next = '''
+tell application "iTunes" to set itunesState to (player state as text)
+tell application "MPlayerX" to set mplayerxState to ((playstatus) as text)
+if itunesState is equal to "playing" then
+	tell application "iTunes" to next track
+else if mplayerxState is equal to "Playing" then
+	tell application "MPlayerX" to goto next episode
+end if
+'''
+
+previous = '''
+tell application "iTunes" to set itunesState to (player state as text)
+tell application "MPlayerX" to set mplayerxState to ((playstatus) as text)
+if itunesState is equal to "playing" then
+	tell application "iTunes" to previous track
+else if mplayerxState is equal to "Playing" then
+	tell application "MPlayerX" to goto previous episode
+end if
+'''
+
+toggle_fullscreen = '''
+activate application "MPlayerX"
+tell application "System Events"
+keystroke "f"
+end tell
+'''
+
+toggle_subtitle = '''
+activate application "MPlayerX"
+tell application "System Events"
+keystroke "s"
+end tell
+'''
+
+lastPaused = ""
 
 while 1:
 
@@ -10,17 +65,24 @@ while 1:
 		print command
 
 	if command == 'play':
-		os.system('osascript play.scpt')
+		cmd = "osascript -e '%s'" % (play_pause % lastPaused)
+		lastPaused = os.popen(cmd).read().rstrip()
 
 	elif command == 'next':
-		os.system('osascript next.scpt')
+		os.system("osascript -e '%s'" % next)
 
 	elif command == 'previous':
-		os.system('osascript previous.scpt')
+		os.system("osascript -e '%s'" % previous)
 
 	elif command == 'vup':
 		os.system('osascript vup.scpt')
 
 	elif command == 'vdown':
 		os.system('osascript vdown.scpt')
+
+	elif command == 'fullscreen':
+		os.system("osascript -e '%s'" % toggle_fullscreen)
+		
+	elif command == 'subtitle':
+		os.system("osascript -e '%s'" % toggle_subtitle)
 
